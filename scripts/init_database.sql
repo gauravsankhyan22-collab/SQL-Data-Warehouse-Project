b5c9f6d594b66648a5c7996aa4510301ec87455c
gauravsankhyan22-collab <gauravsankhyan22@gmail.com>
2026-06-25 20:04:56 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -18,3 +18,22 @@ GO
 
 --Drop and recreate the 'DataWarehouse' database
 IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN ALTER DATABASE DataWArehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE ;
+END;
+GO
+
+--Create the 'DataWarehouse'database 
+CREATE DATABASE DataWarehouse ;
+GO
+
+USE DataWarehouse;
+GO
+
+--Create Schemas
+CREATE SCHEMA bronze ;
+GO
+
+CREATE SCHEMA silver ;
+GO
+
+CREATE SCHEMA gold ;
